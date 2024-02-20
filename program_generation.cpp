86ac47bcf9833586e9b3ef24164481e94702ceae
TLindauer <tiernanxklindauer@gmail.com>
2024-02-20 09:02:57 -0600

    generative program

---
+++ program_generation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//void writeHelloWorldToOutput
+
+int main() {
+    using namespace std;
+    cout << "#include <iostream>" << endl;
+    cout << "int main() {" << endl;
+    cout << "    using namespace std;" << endl;
+    cout << "    cout << \"Hello, world!\" << endl;" << endl;
+    cout << "    return 0;" << endl;
+    cout << "}" << endl;
+    // Could write to file, but that's for later.
+    return 0;
+}
