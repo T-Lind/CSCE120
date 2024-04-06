2fe5de71c0efc4bac8ad9b788bcf20e89bfa0bc8
TLindauer <tiernanxklindauer@gmail.com>
2024-04-06 17:36:52 -0500

    working on fixing this stuff, 46%

--- crazy_8s/Card.cpp
+++ crazy_8s/Card.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
-
+#include <cctype>
 #include "Card.h"
 
 using std::string;
 
-Card::Card(string rank, string suit) /* TODO: initialize */ {
+Card::Card(string rank, string suit) : rank(rank), suit(suit), timesPlayed(0) {
     if (rank.empty() || suit.empty()) {
         throw std::invalid_argument("Rank and suit must not be empty");
     }
-    this->rank = rank;
-    this->suit = suit;
+
+    for (char c : rank) {
+        if (!std::isalnum(c)) {
+            throw std::invalid_argument("Rank must only contain alphanumeric characters");
+        }
+    }
+
+    for (char c : suit) {
+        if (!std::isalnum(c)) {
+            throw std::invalid_argument("Suit must only contain alphanumeric characters");
+        }
+    }
 }
 
 string Card::getRank() {
--- crazy_8s/Player.cpp
+++ crazy_8s/Player.cpp
@@ -8,8 +8,8 @@ using std::vector, std::string, std::size_t;
 Player::Player(bool isAI) : isAI(isAI), hand({}) {
 }
 
-void Player::addToHand(Card* c) {
-  hand.push_back(c);
+void Player::addToHand(Card *c) {
+    hand.push_back(c);
 }
 
 size_t Player::getHandSize() {
@@ -27,17 +27,63 @@ std::string Player::getHandString() {
     return ss.str();
 }
 
-Card* Player::playCard(vector<string> const& suits, string& currentRank, string& currentSuit) {
-    for (size_t i = 0; i < hand.size(); i++) {
-        if (hand.at(i)->canBePlayed(currentRank, currentSuit)) {
-            Card* c = hand.at(i);
-            hand.erase(hand.begin() + i);
-            c->play();
-            currentRank = c->getRank();
-            currentSuit = c->getSuit();
-            return c;
+Card *Player::playCard(vector<string> const &suits, string &currentRank, string &currentSuit) {
+    if (isAI) {
+    } else {
+        while (true) {
+            std::cout << "Your hand contains: " << getHandString() << "\n";
+            std::cout << "The next card played must be a " << currentRank << " or " << currentSuit << "\n";
+            std::cout << "What would you like to play? (enter \"draw card\" to draw a card)\n";
+            string input;
+            std::getline(std::cin, input);
+
+            if (input == "draw card") {
+                return nullptr;
+            }
+            for (auto it = hand.begin(); it != hand.end(); it++) {
+                std::istringstream iss(input);
+                std::string inputRank, inputSuit;
+                iss >> inputRank >> inputSuit;
+
+                if ((*it)->getRank() == inputRank && (*it)->getSuit() == inputSuit) {
+                    if ((*it)->canBePlayed(currentRank, currentSuit)) {
+                        Card *cardToPlay = *it;
+                        hand.erase(it);
+                        cardToPlay->play();
+                        currentRank = cardToPlay->getRank();
+                        currentSuit = cardToPlay->getSuit();
+                        if (currentRank == "8") {
+                            while (true) {
+                                std::cout << "What suit would you like to declare?\n";
+                                string newSuit;
+                                std::getline(std::cin, newSuit);
+
+                                bool found = false;
+                                for (const auto &suit: suits) {
+                                    if (suit == newSuit) {
+                                        found = true;
+                                        break;
+                                    }
+                                }
+
+                                if (found) {
+                                    currentSuit = newSuit;
+                                    break;
+                                } else {
+                                    std::cout << "That's not a suit in this deck. Try again.\n";
+                                }
+                            }
+                        }
+                        return cardToPlay;
+                    } else {
+                        std::cout << "You can't play that card. Try again.\n";
+                        return nullptr;
+                    }
+                }
+            }
+            std::cout << "That's not a card you have. Try again.\n";
+
         }
     }
-
     return nullptr;
 }
