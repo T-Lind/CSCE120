128b4d7d80df521e1a4656d0d0f1fef8c3c910db
TLindauer <tiernanxklindauer@gmail.com>
2024-04-30 11:37:45 -0500

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# CSCE120
+Code that I've written throughout the semester at TAMU.
+
+It's a really boring and easy class, but figure I'd put this up for anyone trying to learn.
+
+Cheers,
+
+<t></t>T-Lind
--- final/Animal.cpp
+++ final/Animal.cpp
@@ -1,26 +1,1 @@
-//
-// Created by tenant on 4/30/2024.
-//
-
-#include "Animal.h"
-
-Animal::Animal(string name, int age) {
-    this->name = name;
-    this->age = age;
-    health = 100;
-}
-
-int Animal::getAge() { return age; }
-void Animal::setAge(int age) { this->age = age; }
-void Animal::setHealth(int health) { this->health = health; }
-int Animal::getHealth() { return health; }
-
-Dog::Dog(int age) : Animal("Dog", age) {}
-void Dog::eat() { setHealth(getHealth() + 5); }
-void Dog::run() { setHealth(getHealth() - 5); }
-void Dog::newBirthday() { age += 1; }
-void Dog::setHealth(int health) {
-    if (health < 0) { Animal::setHealth(0); }
-    else if (health > 10) { Animal::setHealth(10); }
-    else { Animal::setHealth(health); }
-}
+//// Created by tenant on 4/30/2024.//#include "Animal.h"Animal::Animal(string name, int age) {    this->name = name;    this->age = age;    health = 100;}int Animal::getAge() { return age; }void Animal::setAge(int age) { this->age = age; }void Animal::setHealth(int health) { this->health = health; }int Animal::getHealth() { return health; }Dog::Dog(int age) : Animal("Dog", age) {}void Dog::eat() { setHealth(getHealth() + 5); }void Dog::run() { setHealth(getHealth() - 5); }void Dog::newBirthday() { age += 1; }void Dog::setHealth(int health) {    if (health < 0) { Animal::setHealth(0); }    else if (health > 10) { Animal::setHealth(10); }    else { Animal::setHealth(health); }}
