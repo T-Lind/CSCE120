c038ad11b73cfe16dc7fe91b4e3d57cb67dc8674
TLindauer <tiernanxklindauer@gmail.com>
2024-05-01 21:54:18 -0500

    cycle detection

--- final/LinkedList.h
+++ final/LinkedList.h
@@ -5,21 +5,34 @@
 #ifndef CSCE120_LINKEDLIST_H
 #define CSCE120_LINKEDLIST_H
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 struct Node {
     int value;
-    Node* next;
+    Node *next;
+
     Node(int num = 0) : value{num}, next{nullptr} {}
 };
+
 class LinkedList {
-    Node* head;
 
 public:
     LinkedList() : head{nullptr} {}
-    LinkedList(const LinkedList& other) : head{nullptr} {
+
+    LinkedList(int *init, int size) {
+        for (int i = 0; i < size; i++) {
+            this->insertAtEnd(init[i]);
+        }
+    }
+
+    LinkedList(const LinkedList &other) : head{nullptr} {
         if (other.head) {
             head = new Node(other.head->value);
-            Node* current = head;
-            Node* otherCurrent = other.head->next;
+            Node *current = head;
+            Node *otherCurrent = other.head->next;
             while (otherCurrent) {
                 current->next = new Node(otherCurrent->value);
                 current = current->next;
@@ -27,13 +40,14 @@ public:
             }
         }
     }
-    LinkedList& operator=(const LinkedList& other) {
+
+    LinkedList &operator=(const LinkedList &other) {
         if (this != &other) {
             clear();
             if (other.head) {
                 head = new Node(other.head->value);
-                Node* current = head;
-                Node* otherCurrent = other.head->next;
+                Node *current = head;
+                Node *otherCurrent = other.head->next;
                 while (otherCurrent) {
                     current->next = new Node(otherCurrent->value);
                     current = current->next;
@@ -43,61 +57,102 @@ public:
         }
         return *this;
     }
+
     ~LinkedList() {
         clear();
     }
+
     void insertAtBeginning(int value) {
-        Node* newNode = new Node(value);
+        Node *newNode = new Node(value);
         newNode->next = head;
         head = newNode;
     }
+
     void insertAtEnd(int value) {
-        Node* newNode = new Node(value);
+        Node *newNode = new Node(value);
         if (head == nullptr) {
             head = newNode;
         } else {
-            Node* current = head;
-            while (current->next) { current = current->next; }
+            Node *current = head;
+            while (current->next != nullptr) { current = current->next; }
             current->next = newNode;
         }
     }
+
     void remove(int value) {
         if (head == nullptr) { return; }
         if (head->value == value) {
-            Node* temp = head;
+            Node *temp = head;
             head = head->next;
             delete temp;
             return;
         }
-        Node* current = head;
+        Node *current = head;
         while (current->next && current->next->value != value) {
             current = current->next;
         }
         if (current->next) {
-            Node* temp = current->next;
+            Node *temp = current->next;
             current->next = current->next->next;
             delete temp;
         }
     }
+
+    void reverse() {
+        Node *prev = nullptr;
+        Node *current = head;
+        Node *next;
+
+        while (current != nullptr) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+
+        head = prev;
+    }
+
+    bool detectCycle(){
+        vector<Node*> visited;
+
+        Node *current = head;
+        while(current != nullptr){
+            for(Node* node : visited){
+                if(node == current){
+                    return true;
+                }
+            }
+            visited.push_back(current);
+            current = current->next;
+        }
+        return false;
+    }
+
     void clear() {
-        Node* current = head;
+        Node *current = head;
         while (current) {
-            Node* temp = current;
+            Node *temp = current;
             current = current->next;
             delete temp;
         }
         head = nullptr;
     }
+
     void print() const {
-        Node* current = head;
-        while (current) {
+        Node *current = head;
+        if (head == nullptr)
+            return;
+        cout << "printing" << endl;
+        while (current != nullptr) {
             std::cout << current->value << " ";
             current = current->next;
         }
         std::cout << std::endl;
     }
-};
 
+    Node *head = nullptr;
+};
 
 
 #endif //CSCE120_LINKEDLIST_H
--- final/main.cpp
+++ final/main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 #include <cstring>
 
-#include "Animal.h"
+//#include "Animal.h"
+#include "LinkedList.h"
 
 int main() {
-    Dog* a = new Dog(10);
+//    Dog* a = new Dog(10);
+//
+//    a->run();
+//    cout << a->getName() << ": health: " << a->getHealth();
+//    a->newBirthday();
+//    cout << ", age: " << a->getAge() << endl;
+//
+//    delete a;
 
-    a->run();
-    cout << a->getName() << ": health: " << a->getHealth();
-    a->newBirthday();
-    cout << ", age: " << a->getAge() << endl;
+    cout << "Starting..." << endl;
 
-    delete a;
+    int* init = new int[]{1, 4, 6, 2, 3};
+
+
+    auto* ll = new LinkedList(init, 5);
+
+    ll->print();
+    ll->reverse();
+    ll->print();
+
+    // Test out cycle detection
+//    ll->head->next->next->next->next->next = ll->head->next->next;
+    cout << "Cycle detected: " << ll->detectCycle() << endl;
+
+   delete[] init;
+   delete ll;
 }
